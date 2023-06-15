48c16f477fc92ff059fd29fd119feaf5f85c1d27
User <eco-64@mail.ru>
2023-06-15 21:29:15 +0500

    task is solved

--- src/keyboard.py
+++ src/keyboard.py
@@ -1,49 +1,34 @@
 from src.item import Item
-class MixinLog:
-    """Класс MixinLog для добавления в класс Keyboard
+
+class MixinLang:
+    """Класс MixinLang для добавления в класс Keyboard
        функционала по хранению и изменению языка
        (раскладки клавиатуры) без изменения его основной структуры"""
 
-    LANG = "EN"
 
-    def init(self):
-        super().__init__()
-        self.__language = self.LANG
-
-
-    def __str__(self):
-        """Метод для отображения информации об объекте класса (о языке (раскладки клавиатуры) для пользователей"""
-        return self.__language
+    def __init__(self, name, price, quantity):
+        super().__init__(name, price, quantity)
+        self.__language = "EN"
 
     def change_lang(self):
         if self.__language == "EN":
-            return self.__language == "RU"
-        if self.__language == "RU":
-            return self.__language == "EN"
+            self.__language = "RU"
+        else:
+            self.__language = "EN"
+        return self
 
     @property
-    def language(self):
-        return self.LANG
+    def language(self) -> str:
+        """Геттер для атрибута language"""
+        return self.__language
 
 
-    @language.setter
-    def language(self, lang):
-        if lang != "EN":
-            if lang != "RU":
-                print("AttributeError: property 'language' of 'KeyBoard' object has no setter")
-
-
-class Keyboard(Item, MixinLog):
+class Keyboard(MixinLang, Item):
     """Класс для представления товара Keyboard в магазине."""
     def __init__(self, name, price, quantity):
         """
         Создание экземпляра класса Keyboard в результате
-        множественного наследования классов Item и MixinLog.
+        множественного наследования классов Item и MixinLang.
         """
         super().__init__(name, price, quantity)
-        self.name = name
 
-
-    def __str__(self) -> str:
-        """Метод для отображения информации об объекте класса для пользователей"""
-        return self.name
